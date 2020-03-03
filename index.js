565bd3c2badd8a19deb88651ffe24bba52940d90
KHCode <hillkri@oregonstate.edu>
2020-03-03 14:24:51 -0800

    refactored people and research pages and file structure for images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-
+var Member = require("./models/member");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -11,8 +11,72 @@ app.get("/", function(req, res){
 	res.render("landing");
 });
 
+var labTitles = ['Principle Investigator', 'Postdoctoral Researchers', 'Graduate Students', 'Undergraduate Students'];
+var former = ['Postdoctoral Researchers', 'Graduate Students', 'Undergraduate Students'];
+var Member = [
+	{name: 'Dr. Colin Johnson',
+	image: '/images/Johnson Base Final.jpg',
+	title: 'Principle Investigator',
+	current: true,
+	credentials: 'Ph.D 2005 - University of Illinois, Urbana Champaign; Postdoc 2005-2007 - University of Pennsylvania; Postdoc 2007-2011 - University of Wisconsin, Madison',
+	bio: 'Colin is a fan of running, comics, cycling, and beans.'},
+	{name: 'Dr. Josephine Bonventre',
+	image: '/images/bonventre1.jpg',
+	title: 'Postdoctoral Researchers',
+	current: true,
+	credentials: 'This section is for credentials.',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Aayushi Manchanda',
+	image: '/images/manchanda.jpg',
+	title: 'Graduate Students',
+	current: true,
+	credentials: 'PhD. Candidate - Molecular & Cellular Biology',
+	bio: 'Aayushi enjoys puppies and protein pull downs'},
+	{name: 'Shauna Otto',
+	image: '/images/otto1.jpg',
+	title: 'Graduate Students',
+	current: true,
+	credentials: 'PhD. Candidate - Biochemistry & Biophysics',
+	bio: 'Shauna enjoys crafting, karaoke, and membrane transport.'},
+	{name: 'James Miyasaki',
+	image: '/images/miyasaki.png',
+	title: 'Graduate Students',
+	current: true,
+	credentials: 'Master\'s Student - Biochemistry & Biophysics',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Sabrina (Jou-Ying) Lin',
+	image: '',
+	title: 'Graduate Students',
+	current: true,
+	credentials: 'Master\'s Student - Biochemistry & Biophysics',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Tanushri Kumar',
+	image: '/images/kumar.jpg',
+	title: 'Undergraduate Students',
+	current: true,
+	credentials: 'Senior - Biochemistry & Molecular Biology',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Rebecca France',
+	image: '/images/france.jpg',
+	title: 'Undergraduate Students',
+	current: true,
+	credentials: 'Junior - Biochemistry & Molecular Biology',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Susmitha Matlapudi',
+	image: '/images/matlapudi.png',
+	title: 'Undergraduate Students',
+	current: true,
+	credentials: 'Senior - Microbiology',
+	bio: 'This section is for a mini-bio.'},
+	{name: 'Chapman Kuykendall',
+	image: '/images/kuykendall.jpg',
+	title: 'Undergraduate Students',
+	current: true,
+	credentials: 'Junior - Biochemistry & Molecular Biology',
+	bio: 'Chapman is a junior BMB major originally from Phoenix, Oregon. He enjoys reading, hiking, cooking, and irritating his cat, Gato.'},
+];
 app.get("/people", function(req, res){
-	res.render("people");
+	res.render("people", {labTitles: labTitles, former: former, member: Member});
 })
 
 app.get("/papers", function(req, res){
@@ -27,5 +91,5 @@ app.get("/contact", function(req, res){
 	res.render("contact");
 })
 app.listen(3000, function(){
-	console.log("Now serving Yelp Camp!");
+	console.log("Now serving Johnson Lab on the web!");
 });
---
+++ models/member.js
@@ -0,0 +1,14 @@
+var Member = [
+	{name: 'Dr. Colin Johnson',
+	image: '/images/Johnson Base Final.jpg',
+	title: 'Principle Investigator',
+	current: true,
+	credentials: 'Ph.D 2005 - University of Illinois, Urbana Champaign; Postdoc 2005-2007 - University of Pennsylvania; Postdoc 2007-2011 - University of Wisconsin, Madison',
+	bio: 'Colin is a fan of running, comics, cycling, and beans.'},
+	{name: 'Dr. Josephine Bonventre',
+	image: '/images/bonventre1.jpg',
+	title: 'Postdoctoral Researchers',
+	current: true,
+	credentials: 'This section is for credentials.',
+	bio: 'This section is for a mini-bio.'}
+];
