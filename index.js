4315bfa4252b00bce626533591f27bc3c05e59e6
KHCode <hillkri@oregonstate.edu>
2020-03-23 17:11:03 +0000

    goorm local commit before pull from origin

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var Member = require("./models/member");
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+	host: '0.0.0.0',
+	sendmail: true,
+    newline: 'unix',
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -258,7 +265,7 @@ var papers = [
 
 app.get("/people", function(req, res){
 	res.render("people", {labTitles: labTitles, former: former, member: Member, formerMember: formerMember, });
-})
+});
 
 app.get("/papers", function(req, res){
 	res.render("papers", {papers: papers});
@@ -266,11 +273,36 @@ app.get("/papers", function(req, res){
 
 app.get("/research", function(req, res){
 	res.render("research");
-})
+});
 
 app.get("/contact", function(req, res){
 	res.render("contact");
-})
+});
+
+app.post("/contact", function(req, res){
+	var name = req.body.name
+  	var email = req.body.email
+  	var message = req.body.message
+  	var content = `name: ${name} \n email: ${email} \n message: ${message} `
+	mail = {
+		to: 'kristopher.hill@gmail.com',
+		subject: 'New Message from Johnson Lab Online',
+		body: content
+	}
+	
+	transporter.sendMail(mail, function(err, data){
+		if(err){
+			console.log(err);
+		    console.log(info.envelope);
+    		console.log(info.messageId);
+		}else{
+			console.log("Email sent: " + info.response);
+		}
+	})
+	console.log(req.body);
+	res.redirect("/contact");
+});
+
 app.listen(3000, function(){
 	console.log("Now serving Johnson Lab on the web!");
 });
