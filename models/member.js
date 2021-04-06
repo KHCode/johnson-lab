dde1b98fd0e14b6655ca3d0a49a9ad62918f092e
KHCode <hillkri@oregonstate.edu>
2021-04-06 07:22:32 -0700

    refactor member class and root styles

--- models/member.js
+++ models/member.js
@@ -1,15 +1,29 @@
-var Member = [
-	{name: 'Dr. Colin Johnson',
-	image: '/images/Johnson Base Final.jpg',
-	title: 'Principle Investigator',
-	current: true,
-	credentials: 'Ph.D 2005 - University of Illinois, Urbana Champaign; Postdoc 2005-2007 - University of Pennsylvania; Postdoc 2007-2011 - University of Wisconsin, Madison',
-	bio: 'Colin is a fan of running, comics, cycling, and beans.'},
-	{name: 'Dr. Josephine Bonventre',
-	image: '/images/bonventre1.jpg',
-	title: 'Postdoctoral Researchers',
-	current: true,
-	credentials: 'This section is for credentials.',
-	bio: 'This section is for a mini-bio.'}
-];
+const Member = class {
+	name;
+	image;
+	title;
+	current;
+	credentials;
+	bio;
+	research_topic;
+	research_url;
+	current_job;
+	current_job_url;
+	osu_years;
 
+	constructor(name, image, title, current, credentials, bio, research_topic, research_url, current_job, current_job_url, osu_years){
+		this.name = name;
+		this.image = image;
+		this.title = title;
+		this.current = current;
+		this.credentials = credentials;
+		this.bio = bio;
+		this.research_topic = research_topic;
+		this.research_url = research_url;
+		this.current_job = current_job;
+		this.current_job_url = current_job_url;
+		this.osu_years = osu_years;
+	}
+}
+
+export default Member;
